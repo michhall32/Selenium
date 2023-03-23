a292316887ccf6ab08bf940f2d5bd0155d99ec72
Michal Stapor <127783960+michhall32@users.noreply.github.com>
2023-03-23 16:25:50 +0100

    Refactored code - added functions

--- CookieClicker/CookieClicker.py
+++ CookieClicker/CookieClicker.py
@@ -7,6 +7,22 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+SHOP_ITEMS = 2
+COOKIES_CLICKS = 1000
+
+# Getting an amount from the selector
+def getCount(selector):
+    try:
+        result = int(selector.text.split(' ')[0])
+        return result
+    except:
+        return 0
+
+# Waiting for the element to be present on the website
+def is_element_present(id_name):
+     elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, id_name)))
+     return elem
+
 #Chrome web browser options
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -22,34 +38,41 @@ cookiesConsent = WebDriverWait(driver, 10).until(
 cookiesConsent.click()
 
 # Choosing a language - waiting till the element appears
-language = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "langSelect-EN")))
+language = is_element_present("langSelect-EN")
 language.click()
 
 
 # Finding multiple items from the shop
 items =[]
-for i in range(1,-1,-1):
-    items.append(WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, f'productPrice{i}'))))
+for i in range(SHOP_ITEMS,-1,-1):
+    items.append(is_element_present(f'productPrice{i}'))
 
 driver.implicitly_wait(3)
 
-# Clicking the "Big cookie"
-for i in range(1000):
-    cookie = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'bigCookie')))
-    cookie.click()
-    
-    # Checking the cookie count
-    for item in items:
-        cookie_count = driver.find_element(By.XPATH, "(//div[@id='cookies'])[1]")
-        current_count = int(cookie_count.text.split(' ')[0])
-        price = int(item.text)
 
-        # Checking if there is enough cookies to buy an item
-        if current_count > price:
+# Clicking the "Big cookie"
+for i in range(COOKIES_CLICKS):
+    cookie = is_element_present('bigCookie')
+    cookie.click()
+    cookie_count = driver.find_element(By.ID, "cookies")
+    current_count = getCount(cookie_count)
+
+
+    # Checking if there is enough cookies to buy an item
+    for item in items:
+        price = getCount(item)
+
+        if current_count >= price and price > 0:
                 actions = ActionChains(driver)
                 actions.click(item)
                 actions.perform()
 
 
-print(cookie_count.text)
+# Printing the results
+for i in range(SHOP_ITEMS):
+     item_name = driver.find_element(By.ID, f'productName{i}')
+     item_amount = driver.find_element(By.ID, f'productOwned{i}')
+     print(f'{item_name.text}: {item_amount.text}')
+
+
+print(f'\nAmount of cookies left: {cookie_count.text}')
